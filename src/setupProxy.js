a05cbda88596d36a26f38900d125ed05089a07d7
MohamadAlkahil <37299254+MohamadAlkahil@users.noreply.github.com>
2024-05-13 14:34:28 -0400

    proxy update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,13 +1,3 @@
-// const { createProxyMiddleware } = require('http-proxy-middleware');
-
-// module.exports = function (app) {
-//     app.use('/auth/**', 
-//         createProxyMiddleware({ 
-//             target: 'http://localhost:5000'
-//         })
-//     );
-// };
-
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
