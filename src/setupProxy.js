a32278dfb1eb804c83825130f75ec83b61a87233
MohamadAlkahil <37299254+MohamadAlkahil@users.noreply.github.com>
2024-05-13 00:06:14 -0400

    proxy update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -12,14 +12,15 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     // Define the target URL using an environment variable
-    const frontendUri = process.env.SPOTIFY_FRONTEND_URI || 'https://spotify-top-items-18b9b4ba5a53.herokuapp.com';
+    const redirectUri = process.env.SPOTIFY_REDIRECT_URI;
 
     // Proxy requests to '/spotify' routes to the target URL
     app.use('/spotify/**', 
         createProxyMiddleware({ 
-            target: frontendUri,
+            target: redirectUri,
             changeOrigin: true // Needed for Heroku
         })
     );
 };
+
 
